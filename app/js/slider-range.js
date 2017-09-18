937aaed8b27d330c6e4cce4197dcf980d35b3b8d
Kirill Bubentsov <kirill.dm6@gmail.com>
2017-09-18 20:37:50 +0300

    catalog

---
+++ app/js/slider-range.js
@@ -0,0 +1,21 @@
+// var Range = function(){
+//     var range = function(){
+//         $( "#slider-3" ).slider({
+//             range:true,
+//             min: 0,
+//             max: 500,
+//             values: [ 35, 200 ],
+//             slide: function( event, ui ) {
+//                 $( "#price" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+//             }
+//         });
+//     };
+//     return {
+//         initRange: function(){
+//             range();
+//         },
+//         init: function () {
+//             this.initRange()
+//         }
+//     }
+// }();
